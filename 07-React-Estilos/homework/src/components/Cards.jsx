918fafa137459de40aceb0ebf5281df8fbc33d34
Alejandro <alemank23k@gmail.com>
2022-05-10 16:28:56 -0300

    finish homework 07

--- 07-React-Estilos/homework/src/components/Cards.jsx
+++ 07-React-Estilos/homework/src/components/Cards.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Card from './Card.jsx';
+import estilos from './Cards.module.css'
 
-export default function Cards(props) {
+export default function Cards({cities}) {
   // acá va tu código
   // tip, podés usar un map
-  return <div>Cards Component</div>
+  if(!cities) { 
+    return <h1>No hay ciudades disponibles</h1>;
+  }
+  return (
+  <div className={estilos.contenedor}>
+    {
+    cities && cities.map(c =>
+      <Card
+          max={c.main.temp_max}
+          min={c.main.temp_min}
+          name={c.name}
+          img={c.weather[0].icon}
+          onClose={() => alert(c.name)}
+          key={c.id}
+      />)}
+  </div>)
 };
