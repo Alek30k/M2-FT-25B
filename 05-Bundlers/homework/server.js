381d420922fa9a15f95bf621fea13dcc4af8ce02
Alejandro <alemank23k@gmail.com>
2022-05-10 16:30:01 -0300

    finish homework 05

--- 05-Bundlers/homework/server.js
+++ 05-Bundlers/homework/server.js
@@ -14,32 +14,31 @@ var io = socketio(server);
 
 var inMemoryDrawHistory = [];
 
-server.listen(1337, function () {
-  console.log('The server is listening on port 1337!');
+server.listen(1337, function() {
+    console.log('The server is listening on port 1337!');
 });
 
 app.use(express.static(path.join(__dirname, 'browser')));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-io.on('connection', function (socket) {
-  console.log(socket.id, 'connected');
+io.on('connection', function(socket) {
+    console.log(socket.id, 'connected');
 
-  socket.emit('load', inMemoryDrawHistory);
+    socket.emit('load', inMemoryDrawHistory);
 
-  socket.on('draw', function (start, end, color) {
-    inMemoryDrawHistory.push({
-      start: start,
-      end: end,
-      color: color
+    socket.on('draw', function(start, end, color) {
+        inMemoryDrawHistory.push({
+            start: start,
+            end: end,
+            color: color
+        });
+        socket.broadcast.emit('draw', start, end, color);
     });
-    socket.broadcast.emit('draw', start, end, color);
-  });
 
-  socket.on('disconnect', function () {
-    console.log('Goodbye, ', socket.id, ' :(');
-  });
+    socket.on('disconnect', function() {
+        console.log('Goodbye, ', socket.id, ' :(');
+    });
 });
-
